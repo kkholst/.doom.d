46153b186bd3f7c594d0dc49b55d0fc5d89ebe8b
Klaus Holst <klaus@holst.it>
2023-10-02 17:42:19 +0200

    .

--- packages.el
+++ packages.el
@@ -60,6 +60,22 @@
 (package! org-project
   :recipe (:host github :repo "delehef/org-project"))
 
+
+;; (package! pdf-tools :recipe
+;;           (:host github
+;;                  :repo "dalanicolai/pdf-tools"
+;;                  :branch "pdf-roll"
+;;                  :files ("lisp/*.el"
+;;                          "README"
+;;                          ("build" "Makefile")
+;;                          ("build" "server")
+;;                          (:exclude "lisp/tablist.el" "lisp/tablist-filter.el"))))
+
+;; (package! image-roll :recipe
+;;           (:host github
+;;                  :repo "dalanicolai/image-roll.el"))
+
+
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
 
