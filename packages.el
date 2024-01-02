c7dde0008d754284e84a90a647fd44a4108c759a
Klaus Holst <klaus@holst.it>
2024-01-02 23:19:49 +0100

    enable platformio again

--- packages.el
+++ packages.el
@@ -34,6 +34,7 @@
 (package! flycheck-google-cpplint)
 (package! flycheck-clang-analyzer)
 ;; (package! processing-mode)
+(package! arduino-mode)
 (package! platformio-mode)
 (package! org-fragtog)
 ;;(package! org-ref)
